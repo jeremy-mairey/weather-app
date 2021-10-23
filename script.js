f2272387af618ae03e6dd6892efa8eb8a20241ad
j-codepro <j_code@icloud.com>
2021-10-23 16:29:55 +0200

    API working

---
+++ script.js
@@ -0,0 +1,42 @@
+let weather = {
+  apiKey: "ece5ce29eab725da2abdb3930f9785af",
+  fetchWeather: function (city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city-name").innerText = name;
+    document.querySelector(
+      ".icon"
+    ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temps").innerText = Math.round(temp);
+    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").innerText = `Wind: ${speed} km/h`;
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/2048x1080/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", () => {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    weather.search();
+  }
+});
+
+weather.fetchWeather("Paris");
